fae907f086667bdac3b62771c72a0ff80265ae5f
Malte Brieske <9287988+mbrieske@users.noreply.github.com>
2024-12-20 21:17:33 +0000

    day 20

--- src/bin/20.rs
+++ src/bin/20.rs
@@ -1,18 +1,29 @@
 use advent_of_code::AocGrid;
 use glam::IVec2;
 use grid::Grid;
-use pathfinding::prelude::dijkstra;
+use pathfinding::prelude::bfs;
 
 advent_of_code::solution!(20);
 
-fn successors(pos: &IVec2, grid: &Grid<char>) -> Vec<(IVec2, usize)> {
+// fn successors(pos: &IVec2, grid: &Grid<char>) -> Vec<(IVec2, usize)> {
+//     grid.neighbors(*pos)
+//         .filter(|(_, &n)| n != '#')
+//         .map(|(p, _)| (p, 1))
+//         .collect()
+// }
+
+fn successors(pos: &IVec2, grid: &Grid<char>) -> Vec<IVec2> {
     grid.neighbors(*pos)
         .filter(|(_, &n)| n != '#')
-        .map(|(p, _)| (p, 1))
+        .map(|(p, _)| p)
         .collect()
 }
 
-fn solve_part_one(input: &str, min_save: u64) -> Option<u32> {
+fn manhattan_distance(a: IVec2, b: IVec2) -> usize {
+    (a - b).abs().element_sum() as usize
+}
+
+fn solve(input: &str, min_save: usize, max_cheat_picos: usize) -> Option<u32> {
     let maze = Grid::<char>::from_input(input);
     let start = maze
         .indexed_iter()
@@ -26,37 +37,39 @@ fn solve_part_one(input: &str, min_save: u64) -> Option<u32> {
         .map(|((y, x), _)| IVec2::new(x as i32, y as i32))
         .unwrap();
 
-    let (path, cost) = dijkstra(&start, |p| successors(p, &maze), |&p| p == finish).unwrap();
+    // let (path, _) = dijkstra(&start, |p| successors(p, &maze), |&p| p == finish).unwrap();
+    let path = bfs(&start, |p| successors(p, &maze), |&p| p == finish).unwrap();
 
-    let cheats = path.iter().enumerate().flat_map(|(start_cost, p)| {
-        maze.neighbors(*p)
-            .filter(|(_, &n)| n == '#')
-            .map(|(wp, _)| {
-                (
-                    wp,
-                    dijkstra(&wp, |nw| successors(nw, &maze), |&p| p == finish)
-                        .unwrap()
-                        .1,
-                )
-            })
-            .filter_map(move |(wp, finish_cost)| {
-                let save = cost as i64 - (start_cost + finish_cost) as i64 - 1;
-                if save >= min_save as i64 {
-                    Some((wp, save))
-                } else {
-                    None
-                }
-            })
-    });
+    let cheats =
+        path[..path.len() - min_save]
+            .iter()
+            .enumerate()
+            .flat_map(|(from_idx, from_pos)| {
+                path[from_idx + min_save..]
+                    .iter()
+                    .enumerate()
+                    .map(move |(i, to_pos)| {
+                        (
+                            from_idx,
+                            i + from_idx + min_save, // to_idx
+                            manhattan_distance(*to_pos, *from_pos),
+                        )
+                    })
+                    .filter(|&(from_idx, to_idx, dist)| {
+                        let steps = to_idx - from_idx;
+                        let saving = steps - dist;
+                        dist <= max_cheat_picos && saving >= min_save
+                    })
+            });
     Some(cheats.count() as u32)
 }
 
 pub fn part_one(input: &str) -> Option<u32> {
-    solve_part_one(input, 100)
+    solve(input, 100, 2)
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    None
+    solve(input, 100, 20)
 }
 
 #[cfg(test)]
@@ -70,15 +83,15 @@ mod tests {
     #[case(&advent_of_code::template::read_file("examples", DAY), Some(5))]
     fn test_part_one(#[case] input: &str, #[case] expected: Option<u32>) {
         tracing_init(Level::INFO);
-        let result = solve_part_one(input, 20);
+        let result = solve(input, 20, 2);
         assert_eq!(result, expected);
     }
 
     #[rstest]
-    #[case(&advent_of_code::template::read_file("examples", DAY), None)]
+    #[case(&advent_of_code::template::read_file("examples", DAY), Some(7))]
     fn test_part_two(#[case] input: &str, #[case] expected: Option<u32>) {
         tracing_init(Level::INFO);
-        let result = part_two(input);
+        let result = solve(input, 74, 20);
         assert_eq!(result, expected);
     }
 }
